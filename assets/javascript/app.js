d32b6f1794bfc5aa2d8f54182755c1c2e619094b
jfenneyj <jfenney@leadtomarket.com>
2018-05-28 12:12:56 -0700

    update all files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,46 +17,24 @@ var mastQuestions = [ {
 }, {
   question: "What is the name of the animated science fiction comedy-dram film released in 2002 about a Hawaiian girl and her unusual pet?",
   choices: ["Ellen & Froggy", "Attina & Scuttle", "Ariel & Hammy", "Lilo & Stitch"],
+  indexAnswer: 3
+}, {
+  question: "In Beauty and the Beast, what is the name of Gaston's bumbling sidekick?",
+  choices: ["CandleStick", "LeFou", "Bummbles", "McFarley"],
   indexAnswer: 0
 }, {
-  question: "In Sleeping Beauty, who was Princess Aurora betrothed to?",
-  choices: ["Prince Phillip", "Prince Charles", "Prince Henry", "Prince William"],
+  question: "What was the name of the kleptomaniac monkey in the Disney movie Aladdin?",
+  choices: ["Klepto", "MooMoo", "Abu", "Kantar"],
+  indexAnswer: 2
+}, {
+  question: "In which US city was Walt Disney born?",
+  choices: ["Illinois", "Maui", "Miami", "Los Angeles"],
   indexAnswer: 0
 }, {
-  question: "In Sleeping Beauty, who was Princess Aurora betrothed to?",
-  choices: ["Prince Phillip", "Prince Charles", "Prince Henry", "Prince William"],
-  indexAnswer: 0
-}, {
-  question: "In Sleeping Beauty, who was Princess Aurora betrothed to?",
-  choices: ["Prince Phillip", "Prince Charles", "Prince Henry", "Prince William"],
-  indexAnswer: 0
-}, {
-  question: "In Sleeping Beauty, who was Princess Aurora betrothed to?",
-  choices: ["Prince Phillip", "Prince Charles", "Prince Henry", "Prince William"],
+  question: "What are the names of the three fairies in the Disney classic Sleeping Beauty",
+  choices: ["Attina, Aquata, Ursala", "Flora, Fauna and Merryweather", "Hilo, Milo and Dingo", "Aerwyna, Ailsa and Aine"],
   indexAnswer: 0
 },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ];
 
