641c819ccda95933e6b3a80e7d0bf66b4e193676
jfenneyj <jfenney@leadtomarket.com>
2018-05-28 08:08:57 -0700

    updated js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+
+
+
+
+
+
+
+var mastQuestions = [ {
+  question: "In Sleeping Beauty, who was Princess Aurora betrothed to?",
+  choices: ["Prince Phillip", "Prince Charles", "Prince Henry", "Prince William"],
+  indexAnswer: 0
+}, {
+  question: "In The Little Mermaid, what was the name of the human that Ariel falls in love with?",
+  choices: ["King Triton", "Prince Sebastian ", "Prince Eric", "Prince Louis"],
+  indexAnswer: 2
+}, {
+  question: "What is the name of the animated science fiction comedy-dram film released in 2002 about a Hawaiian girl and her unusual pet?",
+  choices: ["Ellen & Froggy", "Attina & Scuttle", "Ariel & Hammy", "Lilo & Stitch"],
+  indexAnswer: 0
+}, {
+  question: "In Sleeping Beauty, who was Princess Aurora betrothed to?",
+  choices: ["Prince Phillip", "Prince Charles", "Prince Henry", "Prince William"],
+  indexAnswer: 0
+}, {
+  question: "In Sleeping Beauty, who was Princess Aurora betrothed to?",
+  choices: ["Prince Phillip", "Prince Charles", "Prince Henry", "Prince William"],
+  indexAnswer: 0
+}, {
+  question: "In Sleeping Beauty, who was Princess Aurora betrothed to?",
+  choices: ["Prince Phillip", "Prince Charles", "Prince Henry", "Prince William"],
+  indexAnswer: 0
+}, {
+  question: "In Sleeping Beauty, who was Princess Aurora betrothed to?",
+  choices: ["Prince Phillip", "Prince Charles", "Prince Henry", "Prince William"],
+  indexAnswer: 0
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
